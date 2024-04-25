2f78ad1a367b6ca6af535c8d55d2cda4d18c6cfb
Gavin Noronha <gavinln@hotmail.com>
2024-04-25 10:50:22 -0700

    test approx equal

--- tests/test_numpy_op.py
+++ tests/test_numpy_op.py
@@ -4,3 +4,9 @@ from github_actions_demo import numpy_op
 def test_sum_int():
     values = list(range(10))
     assert numpy_op.sum(values) == sum(values)
+
+
+def test_sum_float():
+    values = list(range(10))
+    values = [value * 0.3 for value in values]
+    assert numpy_op.sum(values) == sum(values)
